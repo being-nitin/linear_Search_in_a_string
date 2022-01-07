866599c2063708b21e469cb03dc5cbd419cb28f5
Nitin <nsinghboss02@gmail.com>
2022-01-07 10:26:37 +0530

    Initial commit

---
+++ src/com/codechef/Main.java
@@ -0,0 +1,25 @@
+package com.codechef;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        String str = "nitin";
+        System.out.println(str.toCharArray());
+        int ans = search(str, 'i');
+        System.out.println(ans);
+    }
+
+    static int search(String str, char target) {
+        if (str.length() == 0) {
+            return -1;
+        } else {
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == target) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
